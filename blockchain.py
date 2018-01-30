428dd51b7b7287c39a3086e8ad2e306e90e9a365
ANDROID-SA66889\Archon <facu.ch@gmail.com>
2018-01-30 17:08:32 -0300

    First commit, blockchain template

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+class Blockchain(object):
+	def __init__(self):
+		self.chain= []
+		self.current_transactions = []
+
+		def new_block(self):
+			#Crea nuevo bloque y lo agrega a la cadena
+			pass
+
+		def new_transaction(self):
+			#Agrega una nueva transaccion a la lista
+			pass
+
+		@staticmethod
+		def hash(block):
+				#Hashea un bloque
+				pass
+
+		@property
+
+		def last_block(self):
+			#Returns the last Block in the chain
+			pass
